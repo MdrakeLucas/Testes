4faae34a8d6de8f418e2f2ce2d8a599afeca4b1f
MdrakeLucas <106096672+MdrakeLucas@users.noreply.github.com>
2022-06-01 10:05:12 -0300

    Create testes.js

---
+++ testes.js
@@ -0,0 +1,18 @@
+console.log('Hello World')
+var a = 'texto'
+console.log(a)
+console.log(typeof a)
+let nota = 457
+console.log(nota)
+const peso1 = 1.0 
+const peso2 = 4274
+console.log(peso1, peso2)
+console.log(Number.isInteger(peso2))
+
+const Ava1 = 5.78
+const Ava2 = 8.21
+
+const total = Ava1 * peso1 + Ava2 * peso2
+const media = total / (peso1 + peso2)
+console.log(media .toFixed(2))
+
